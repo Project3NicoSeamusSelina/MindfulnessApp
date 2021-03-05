594fc12c1539b9d8ebf817bbb85c026ba8bb6213
Seamus O'Sullivan <info@seamuso.com>
2021-03-05 15:52:30 +0100

    authentication built

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,16 +1,21 @@
-
+import { logout } from '../../services/auth'
 import React, { Component } from 'react'
 // import { Route, Link } from 'react-router-dom';
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
 export default class NavBar extends Component {
+    handleLogout = () => {
+      logout().then(() => {
+        this.props.setUser(null)
+      })
+    }
   render() {
     return (
       <div>
         <Link to="/journal">Journal</Link>
         <Link to="/routine">Daily Routine</Link>
         <Link to="/moodtracker">Mood Tracker</Link>
-        <Link to="/logout">Logout</Link>
+        <Link to="/logout" onClick={() => this.handleLogout()}>Logout</Link>
       </div>
     )
   }
