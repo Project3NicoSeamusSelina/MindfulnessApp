357d8f3cc47fc1dc73091160eafb4a163a48d491
Seamus O'Sullivan <info@seamuso.com>
2021-03-10 21:30:24 +0100

    user id added to Journal

--- routes/index.js
+++ routes/index.js
@@ -43,7 +43,7 @@ router.get('/getSelectedEntry', (req, res, next) => {
 
 router.post('/entries', (req,res, next) => {
   const {date, question1, question2, question3, ratingMood, ratingMotivation} = req.body;
-   JournalEntry.create({date, question1, question2, question3, ratingMood, ratingMotivation})
+   JournalEntry.create({date, question1, question2, question3, ratingMood, ratingMotivation, user: req.user})
     .then(response => res.send(response))
 
     .catch(err=> next(err))
